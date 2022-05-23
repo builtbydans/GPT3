e69735f1973d0b02d899f141ea437026967bfdc0
Danish Shafi <danish.shafi@live.co.uk>
2022-05-23 15:50:24 +0100

    Project complete

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,7 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title="What is GPT3?" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde quas vitae, ea excepturi dolores debitis eum beatae, eligendi, quam corrupti tenetur ducimus harum rem amet nobis vero eos accusamus minus quia. Facilis veniam fuga eligendi laborum repellendus corporis deserunt ex excepturi delectus impedit est rem molestias facere eos."/>
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">Possibilities beyond your imagination</h1>
